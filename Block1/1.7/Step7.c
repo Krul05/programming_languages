055f4301199d6a0e3f596218a7a32f24504c9083
Стеценко Мария <maria@stetsenko.su>
2024-10-22 00:36:23 +0300

    Create Step7.c

---
+++ Block1/1.7/Step7.c
@@ -0,0 +1,12 @@
+void factorize( int n, int* a, int* b )
+{
+  *a = 1;
+  *b  = n;
+  for (int i = 2; i<=n/2; i+=1) {
+      if (n%i==0) {
+          *a = i;
+          *b = n/i;
+          break;
+      }
+  }
+}
